7202be08a07d2b47c54d5b2cfbe8e3d9ace64834
Pumkko <c.coque@hotmail.fr>
2016-10-03 10:26:56 +0800

    Create ParameterPack.cpp
    Use multiple inheritance to deal with parameter pack

---
+++ ParameterPack.cpp
@@ -0,0 +1,35 @@
+
+//Louis Dionne implementation,  Just to help me to remember this technique 
+
+#include <utility>
+template<std::size_t S, typename T>
+struct Element
+{
+    using type = T;
+};
+
+template<typename S, typename... Ts>
+struct Indexer;
+
+template <std::size_t ...Ss, typename ...Ts>
+struct Indexer<std::index_sequence<Ss...>, Ts...>
+    : Element<Ss, Ts>...
+{ };
+
+
+template<std::size_t S, typename T>
+static Element<S,T> select( Element<S, T> );
+
+template<std::size_t S, typename... Ts>
+using type = typename decltype (
+        select<S>( Indexer< std::index_sequence_for<Ts...>, Ts...>{} )
+    )::type;
+
+
+template<typename T>
+struct Check;
+
+int main()
+{
+    Check < type<0, int, float, double> > f;
+}
